dd3d7a002883de244ed7713bcb5c6fe7ac218105
Dragonsbrethren <dragonsbrethren@gmail.com>
2025-02-11 11:50:12 -0500

    stripped code from plotvaluesw and made assumptions to lessen the amount of assembly code needed for each value

--- plotvaluesw.asm
+++ plotvaluesw.asm
@@ -1,30 +1,40 @@
+; This is a stripped down, specialized variation of 7800basic's standard
+; plotvalue subroutine which is designed to print singlewide characters
+; in doublewide mode. Should not be used outside of the menu. It makes
+; assumptions that the standard subroutine does not for the sake of space
+
 plotvaluesw
+; Just front load these since they never change for menu purposes
+;        lda	#<menu_font
+;        sta	temp1
+;        lda	#>menu_font
+;        sta	temp2
+;        lda	charactermode
+;        sta	temp9
+;        lda	#(menu_font_mode | %01100000)
+;        sta	charactermode
+        lda	#30	; width in two's complement
+;        ora	#0	; palette left shifted 5 bits
+        sta	temp3
+
+
          ; calling 7800basic command:
          ; plotvalue digit_gfx palette variable/data number_of_digits screen_x screen_y
          ; ...displays the variable as BCD digits
          ;
          ; asm sub arguments: 
-         ; temp1=lo charactermap
-         ; temp2=hi charactermap
-         ; temp3=palette | width byte
-         ; temp4=x
-         ; temp5=y
-         ; temp6=number of digits
-         ; temp7=lo variable
-         ; temp8=hi variable
-         ; temp9=character mode
+         ; temp1=lo charactermap        <-- hardcoded
+         ; temp2=hi charactermap        <-- unused
+         ; temp3=palette | width byte   <-- hardcoded, used by plotcharacters
+         ; temp4=x                      <-- loaded before jsr
+         ; temp5=y                      <-- loaded before jsr
+         ; temp6=number of digits       <-- unused
+         ; temp7=lo variable            <-- loaded before jsr
+         ; temp8=hi variable            <-- loaded before jsr
+         ; temp9=character mode         <-- unused as far as I can tell
 
-         lda #0
-         tay
+         ldy #0
          ldx valbufend
-
-         lda plotdigitcount
-         and #1
-         beq pvnibble2charsw
-         lda #0
-         sta VALBUFFER,x ; just in case we skip this digit
-         beq pvnibble2char_skipnibblesw
-
 pvnibble2charsw
          ; high nibble...
          lda (temp7),y
@@ -34,10 +44,10 @@ pvnibble2charsw
          lsr
          lsr
          bne pvnibble2charsw_offset
-         lda #CHAR_SPACE
+         lda #CHAR_SPACE        ; omit leading zero
 pvnibble2charsw_offset
          clc
-         adc temp1 ; add the offset to character graphics to our value
+         adc #<menu_font ; add the offset to character graphics to our value
          sta VALBUFFER,x
          inx
          dec plotdigitcount
@@ -47,13 +57,13 @@ pvnibble2char_skipnibblesw
          lda (temp7),y
          and #$0f 
          clc
-         adc temp1 ; add the offset to character graphics to our value
+         adc #<menu_font ; add the offset to character graphics to our value
          sta VALBUFFER,x 
          inx
          iny
 
-         dec plotdigitcount
-         bne pvnibble2charsw
+;         dec plotdigitcount
+;         bne pvnibble2charsw
 
          ;point to the start of our valuebuffer
          clc
