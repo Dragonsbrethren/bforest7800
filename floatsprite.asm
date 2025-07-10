130be0e5e5b4d1f212c70f9c53da23c53964a219
Dragonsbrethren <dragonsbrethren@gmail.com>
2025-07-10 13:23:55 -0400

    river map updates, early framework for loading river tileset maps

---
+++ floatsprite.asm
@@ -0,0 +1,34 @@
+ MAC floatsprite
+ZONETODO   SET {1}   
+ZONEADDR   SET {2}   
+OBJECTTODO SET ({3} * 5)
+
+    ldx dlend+ZONETODO
+
+    lda ZONEADDR+OBJECTTODO+0
+    sta ZONEADDR,x
+    inx
+
+    lda ZONEADDR+OBJECTTODO+1
+    sta ZONEADDR,x
+    inx
+
+    lda ZONEADDR+OBJECTTODO+2
+    and #~(WZONEHEIGHT*2)
+    sta ZONEADDR+OBJECTTODO+2
+    ora #(WZONEHEIGHT*2)
+    sta ZONEADDR,x
+    inx
+
+    lda ZONEADDR+OBJECTTODO+3
+    sta ZONEADDR,x
+    inx
+
+    lda ZONEADDR+OBJECTTODO+4
+    sta ZONEADDR,x
+    inx
+
+    stx dlend+ZONETODO
+
+ ENDM
+
